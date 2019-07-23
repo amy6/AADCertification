0788760566944bc25bc6dcec89ae50005cc27072
Mahima <mahima.6.ms@gmail.com>
2019-07-24 00:19:16 +0530

    add big picture style to updated notification

--- NotifyMe/app/src/main/java/com/example/notifyme/CustomBroadcastReceiver.java
+++ NotifyMe/app/src/main/java/com/example/notifyme/CustomBroadcastReceiver.java
@@ -3,11 +3,16 @@ package com.example.notifyme;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 public class CustomBroadcastReceiver extends BroadcastReceiver {
+
+    public CustomBroadcastReceiver() {
+    }
 
     @Override public void onReceive(Context context, Intent intent) {
         if (intent != null) {
@@ -37,9 +42,21 @@ public class CustomBroadcastReceiver extends BroadcastReceiver {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, MainActivity.NOTIFICATION_CHANNEL_ID)
                 .setContentTitle("Ooh cool! I've been updated.")
                 .setSmallIcon(R.drawable.ic_android)
+                .setLargeIcon(getBitmap(context))
                 .setContentText("Great! You finally actually did something.")
+                .setStyle(createStyle(context))
                 .setAutoCancel(true);
         NotificationManagerCompat.from(context).notify(MainActivity.NOTIFICATION_ID, builder.build());
+    }
+
+    private NotificationCompat.BigPictureStyle createStyle(Context context) {
+        return new NotificationCompat.BigPictureStyle()
+                .bigLargeIcon(getBitmap(context))
+                .bigLargeIcon(null);
+    }
+
+    private Bitmap getBitmap(Context context) {
+        return BitmapFactory.decodeResource(context.getResources(), R.drawable.mascot);
     }
 
     private void startService(Context context) {
